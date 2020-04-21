16fc11c6024d2679c3fd3eeccb56afea0b347eb7
Eduards Birznieks <eduards.latvia@gmail.com>
2020-04-21 15:45:23 +0200

    lock out take

--- siteConfig.js
+++ siteConfig.js
@@ -2,7 +2,7 @@ module.exports = {
   name: 'Retro Back',
   shortName: 'RB',
   description: 'Life is to short to drive a boring car!',
-  url: 'https://gatsby-lam.vaporwavy.io', // Domain of your site without prefix!
+  url: 'https://festive-wescoff-a365e2.netlify.app', // Domain of your site without prefix!
   prefix: '/',
   author: 'eddythedev',
   twitter: ''
