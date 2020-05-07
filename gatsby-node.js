d20ba29cf1533033663a5ff452666b952fd2892e
Eduards Birznieks <eduards.latvia@gmail.com>
2020-05-07 12:57:25 +0200

    Thumbnail string fix atemt to fix

--- gatsby-node.js
+++ gatsby-node.js
@@ -77,7 +77,6 @@ exports.createPages = ({ graphql, actions }) => {
 
 exports.onCreateNode = ({ node, actions, getNode }) => {
   const { createNodeField } = actions
-  fmImagesToRelative(node)
 
   if (node.internal.type === `MarkdownRemark`) {
     const value = createFilePath({ node, getNode })
