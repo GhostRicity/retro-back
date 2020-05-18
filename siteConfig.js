cb0713d2ca6c2e2ba456892408392f164f17ef7f
Eduards Birznieks <eduards.latvia@gmail.com>
2020-05-18 16:18:08 +0200

    Fixing acidental merg issuese

--- siteConfig.js
+++ siteConfig.js
@@ -1,7 +1,7 @@
 module.exports = {
   name: "Retro Back",
   shortName: "RB",
-  description: "This is Gerys Blog!",
+  description: "Life is to short to drive boring cars!",
   url: "https://ArchyStyles.com", // Domain of your site without prefix!
   prefix: "/",
   author: "Eduards Birnzieks",
