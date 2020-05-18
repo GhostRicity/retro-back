7e8a4ac7845592226a63d6c9b0734b7d29433d4e
Eduards Birznieks <eduards.latvia@gmail.com>
2020-05-18 15:52:32 +0200

    Fluid images

--- src/pages/index.js
+++ src/pages/index.js
@@ -69,11 +69,7 @@ const indexQuery = graphql`
             thumbnail {
               childImageSharp {
                 fluid(maxWidth: 1360, quality: 75) {
-                  src
-                  srcSet
-                  aspectRatio
-                  sizes
-                  base64
+                  ...GatsbyImageSharpFluid
                 }
               }
               publicURL
